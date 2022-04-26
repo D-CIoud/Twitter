1eb6e4a0b9c32786060e5a99b2691a1e10b0f471
D-CIoud <36202151+D-CIoud@users.noreply.github.com>
2022-04-26 00:08:17 -0500

    Requerimiento 2 - Práctica 4 - User data in a list

--- app/Services/UserService.js
+++ app/Services/UserService.js
@@ -4,6 +4,10 @@ class userService {
     static create(id,username,name){
         return new User(id, username, name, "Sin Bio")
     }
+
+    static getInfo(User){
+        return [User.id, User.username, User.name, User.bio]
+    }
 }
 
 module.exports = userService
--- test/Services/UserService.test.js
+++ test/Services/UserService.test.js
@@ -1,12 +1,23 @@
+const userService = require('../../app/Services/UserService');
 const UserService = require('../../app/Services/UserService') //Importar la clase UserService
 
 describe("Test's for UserService", () => {
 
     test('1. Create new user using UserService', () => {
-        const user = UserService.create(1,'Jona','Jonathan');
-        expect(user.username).toBe('Jona')
+        const user = UserService.create(1,'Jona','Jonathan')
         expect(user.name).toBe('Jonathan')
         expect(user.id).toBe(1)
         expect(user.bio).not.toBeUndefined()
+    })
+
+    test('2. Get all user data in a list', () => {
+        const user = UserService.create(1,'Jona','Jonathan')
+        const userInfoInList = userService.getInfo(user)
+    
+        expect(userInfoInList[0]).toBe(1)
+        expect(userInfoInList[1]).toBe('Jona')
+        expect(userInfoInList[2]).toBe('Jonathan')
+        expect(userInfoInList[3]).toBe('Sin Bio')
+
     })
 })
