f37fe3b3c38751b11bb7075d381ef48673751e6a
D-CIoud <36202151+D-CIoud@users.noreply.github.com>
2022-04-25 23:35:12 -0500

    Requerimiento 1 - Práctica 4

--- test/Services/UserService.test.js
+++ test/Services/UserService.test.js
@@ -3,9 +3,9 @@ const UserService = require('../../app/Services/UserService') //Importar la clas
 describe("Test's for UserService", () => {
 
     test('1. Create new user using UserService', () => {
-        const user = UserService.create(1,'Jonathan','Jona');
-        expect(user.username).toBe('Jonathan')
-        expect(user.name).toBe('Jona')
+        const user = UserService.create(1,'Jona','Jonathan');
+        expect(user.username).toBe('Jona')
+        expect(user.name).toBe('Jonathan')
         expect(user.id).toBe(1)
         expect(user.bio).not.toBeUndefined()
     })
